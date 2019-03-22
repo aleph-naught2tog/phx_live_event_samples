2574efff7636f0dafc26ec6df0691bc8483b4466
aleph-naught2tog <maggie@8thlight.com>
2019-03-22 18:17:15 -0500

    Adds contrast between bug types
    The first input will show an error in the console; the latter won't show any error, nor will it actually fire any event to the server. The error is on `bindKeys`, so presumably the issue i re binding the event targets; the first one must just end up with no target and the latter a missing one, or rather probably the `document` might have default listeners?

--- lib/phx_live_event_samples_web/live/basic_live.ex
+++ lib/phx_live_event_samples_web/live/basic_live.ex
@@ -7,14 +7,26 @@ defmodule PhxLiveEventSamplesWeb.BasicLive do
         <button phx-click="click">
           Working button
         </button>
+        <hr />
         <label for="input_target_id">
-          (Currently useless) input:
+          This input will error in the JS console on keydown:
         </label>
         <input
           type="text"
           id="input_target_id"
           name="input_target"
           phx-keydown="keydown"
+        />
+        <hr />
+        <label for="input_target_id">
+          This input will simply never fail and never fire on keydown:
+        </label>
+        <input
+          type="text"
+          id="input_target_id"
+          name="input_target"
+          phx-keydown="keydown"
+          phx-target="document"
         />
       </section>
     }
