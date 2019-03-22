0a1fa6804e76cf506f2163bf2adb2cdd22f96e93
aleph-naught2tog <maggie@8thlight.com>
2019-03-22 17:01:01 -0500

    Add live_render import to base view

--- lib/phx_live_event_samples_web.ex
+++ lib/phx_live_event_samples_web.ex
@@ -36,6 +36,9 @@ defmodule PhxLiveEventSamplesWeb do
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
+      # Import convenience functions for LiveView
+      import Phoenix.LiveView, only: [live_render: 2, live_render: 3]
+
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
